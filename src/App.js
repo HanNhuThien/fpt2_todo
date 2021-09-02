a3ef3954a599ad11c57033317464b0a41fd716d6
HanNhuThien <nhung.ntc21@gmail.com>
2021-09-02 19:28:57 +0700

    b3.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import "./App.css";
-import SearchBar from "./UI/SearchBar";
+import Input from "./UI/Input";
 import Button from "./UI/Button";
 import TodoList from "./com/TodoList";
 import { useState } from "react";
 
 const DUMMY_TODO_LIST = [
   { id: 1, title: "title 1", description: "description 1", isDone: false },
-  { id: 2, title: "title 1", description: "description 1", isDone: false },
-  { id: 3, title: "title 1", description: "description 1", isDone: false },
-  { id: 4, title: "title 1", description: "description 1", isDone: false },
+  { id: 2, title: "title 2", description: "description 2", isDone: false },
+  { id: 3, title: "title 3", description: "description 3", isDone: false },
+  { id: 4, title: "title 4", description: "description 4", isDone: false },
 ];
 
 function App() {
   const [todoList, setTodoList] = useState(DUMMY_TODO_LIST);
+  const onUpdateItem = (updatedTodo) => {
+    setTodoList((prevState) => {
+      let prev = [...prevState];
+      const updatedIndex = prev.findIndex((e) => e.id === updatedTodo.id);
 
+      if (updatedIndex === -1) {
+        console.log("updated index invalid");
+        return prev;
+      }
+      prev.splice(updatedIndex, 1, updatedTodo);
+      return prev;
+    });
+  };
+  const onDeleteItem = (todoId) => {
+    setTodoList((prevState) => {
+      let prev = [...prevState];
+      const updatedIndex = prev.findIndex((e) => e.id === todoId);
+      if (updatedIndex === -1) {
+        console.log("updated index invalid");
+        return prev;
+      }
+      prev.splice(updatedIndex, 1);
+      console.log(prev);
+      return prev;
+    });
+  };
   return (
     <div className="App">
-      <SearchBar />
+      <Input />
       <Button>Add todo</Button>
-      <TodoList todoList={todoList} />
+      <TodoList
+        todoList={todoList}
+        onUpdateItem={onUpdateItem}
+        onDeleteItem={onDeleteItem}
+      />
     </div>
   );
 }
--- src/com/TodoItem.js
+++ src/com/TodoItem.js
@@ -1,34 +1,43 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Button from "../UI/Button";
 import Input from "../UI/Input";
 import cz from "./TodoItem.module.css";
 
 const TodoItem = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+  //   const [isDone, setIsDone] = useState(props.todo.todo.isDone);
   const [title, setTitle] = useState(props.todo.title);
   const [description, setDescription] = useState(props.todo.description);
   const [isDone, setIsDone] = useState(props.todo.isDone);
-  const [isEditing, setIsEditing] = useState(false);
-  //   const [isDone, setIsDone] = useState(props.todo.isDone);
 
   const onCheckboxChangeHandler = (e) => {
-    setIsDone(e.checked);
+    //setIsDone(e.checked);
   };
   const onClickEdit = (e) => {
     setIsEditing((prev) => !prev);
   };
   const onClickDelete = (e) => {
-    // setIsDone(e.checked);
+    props.onDeleteItem(props.todo.id);
   };
 
   const onClickSave = (e) => {
     setIsEditing((prev) => !prev);
+    props.onUpdateItem({
+      id: props.todo.id,
+      title: title,
+      description: description,
+      isDone: isDone,
+    });
   };
-  const onClickCancelSave = (e) => {
+  const onClickCancel = (e) => {
     // setIsDone(e.checked);
+    setIsEditing(false);
   };
 
   const onChangeTitle = (e) => {
+    setTitle(e.target.value);
     // setIsDone(e.checked);
+    // ref_title.current.value = e.target.value;
   };
   const onChangeDescription = (e) => {
     // setIsDone(e.checked);
@@ -37,50 +46,72 @@ const TodoItem = (props) => {
   return (
     <React.Fragment>
       {isEditing ? (
-        <div className={`${cz.todoItem} ${cz.todoItem_edit}`}>
+        <form className={`${cz.todoItem} ${cz.todoItem_edit}`}>
           {/* ------------- mode : editing -------------*/}
           <div className={cz.div_title_des}>
             {/* 1. title */}
-            <Input onChange={onChangeTitle}></Input>
+
+            <Input
+              className={cz.todoTitle_edit}
+              placeholder="input title"
+              value={title}
+              onChange={onChangeTitle}
+            ></Input>
 
             {/* 2. description */}
-            <Input onChange={onCheckboxChangeHandler}></Input>
+            <Input
+              className={cz.todoDescription_edit}
+              placeholder="input description"
+              value={description}
+              onChange={onCheckboxChangeHandler}
+            ></Input>
           </div>
           {/* 3. checkbox isDone */}
           <Input
+            className={cz.checkbox}
             type="checkbox"
             checked={isDone}
             onChange={onCheckboxChangeHandler}
           ></Input>
 
           {/* 4. button save/ cancel save */}
-          <div>
-            <Button onClick={onClickSave}>Save</Button>
-            <Button onClick={onClickCancelSave}>Cancel</Button>
+          <div className={`${cz.div_buttons} ${cz.div_buttons_edit}`}>
+            <Button className={cz.button_save} onClick={onClickSave}>
+              Save
+            </Button>
+            <Button className={cz.button_cancel} onClick={onClickCancel}>
+              Cancel
+            </Button>
           </div>
-        </div>
+        </form>
       ) : (
-        <div className={`${cz.todoItem} ${cz.todoItem_notEdit}`}>
-          {/* ----- mode : display (not editing) ----- */}
+        <div className={`${cz.todoItem} ${cz.todoItem_display}`}>
+          {/* ----- mode : display (not editing) -------------- */}
+
           <div className={cz.div_title_des}>
             {/* 1. title */}
-            <h3 className={cz.todoTitle}>{title}</h3>
+            <span className={cz.todoTitle_display}>{title}</span>
 
             {/* 2. description */}
-            <p className={cz.todoDescription}>{description}</p>
+            <span className={cz.todoDescription_display}>{description}</span>
           </div>
 
           {/* 3. checkbox isDone */}
           <Input
+            className={cz.checkbox}
             type="checkbox"
             checked={isDone}
             onChange={onCheckboxChangeHandler}
           ></Input>
 
           {/* 4. button edit / delete*/}
-          <div>
-            <Button onClick={onClickEdit}>Edit</Button>
-            <Button onClick={onClickDelete}>Delete</Button>
+          <div className={`${cz.div_buttons} ${cz.div_buttons_display}`}>
+            <Button className={cz.button_edit} onClick={onClickEdit}>
+              Edit
+            </Button>
+            <Button className={cz.button_delete} onClick={onClickDelete}>
+              Delete
+            </Button>
           </div>
         </div>
       )}
